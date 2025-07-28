9048ce8a7b9db9efb41831571e42e8ef8c673e49
Ankit <hiisatna@gmail.com>
2025-07-28 15:05:42 +0530

    Added test for single number input

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -8,5 +8,9 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an epmty string' do
       expect(calculator.add('')).to eq(0)
     end
+
+    it 'returns the number for a single input number' do
+      expect(calculator.add('1')).to eq(1)
+    end
   end
 end
