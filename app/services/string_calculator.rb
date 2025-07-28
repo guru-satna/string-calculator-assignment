7ca85be63f137c61f7d4338f619d7842d91a991c
Ankit <hiisatna@gmail.com>
2025-07-28 15:28:41 +0530

    Raise error for negative number

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -5,6 +5,9 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.blank?
     
-    numbers.split(DELIMITER).map(&:to_i).sum
+    nums = numbers.split(DELIMITER).map(&:to_i)
+    negative_nums = nums.select(&:negative?)
+    return nums.sum if negative_nums.empty?
+    raise "Negative numbers are not allowed: #{negative_nums.join(', ')}" 
   end
 end
