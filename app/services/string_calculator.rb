f2017f2140de6b7dc0b96c2b4d0663a1964dd40e
Ankit <hiisatna@gmail.com>
2025-07-28 15:31:26 +0530

    Refactor add method

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,13 +1,21 @@
 # app/services/string_calculator.rb
 class StringCalculator
-  DELIMITER = /,|\n|;/
+  DELIMITER = /,|\n|;/.freeze
 
   def add(numbers)
     return 0 if numbers.blank?
-    
+
     nums = numbers.split(DELIMITER).map(&:to_i)
-    negative_nums = nums.select(&:negative?)
+    negative_nums = negative_numbers(nums)
+
     return nums.sum if negative_nums.empty?
+
     raise "Negative numbers are not allowed: #{negative_nums.join(', ')}" 
+  end
+
+  private
+
+  def negative_numbers(nums)
+    nums.select(&:negative?)
   end
 end
